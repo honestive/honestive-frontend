9e11f55e769709028af906f2f0643e5825d8d400
Domas <domas.bitvinskas@me.com>
2018-07-31 15:30:51 +0300

    Implement subscribe; update candour to 3.1.0

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import fluid from '@candour/fluid'
+import step from '@candour/step'
+import gql from 'graphql-tag'
+import { Mutation } from 'react-apollo'
 import {
   Container,
   Headline,
   Text,
-  Input,
   Button,
+  CandourConsumer,
 } from 'candour'
-
+import {
+  Form,
+  Text as TextField,
+} from 'informed'
 import Navbar from './Navbar'
+
+const SUBSCRIBE = gql`
+  mutation subscribe($name: String!, $email: String!) {
+    createSubscriber(data: {
+      name: $name,
+      email: $email,
+    }) {
+      id
+      name
+    }
+  }
+`;
 
 export default () => (
   <div>
@@ -22,17 +40,41 @@ export default () => (
         automatically synchronized with your GitHub repository.
       </Text>
 
-      <Container flex marginTop={2}>
-        <Input name='email' placeholder='Your email' marginRight={2} />
-        <Button>
-          Join early adopters
-        </Button>
-      </Container>
-      <Container flexEnd>
-        <Text paddingTop>
-          Pre-launch. Invites in a few weeks.
-        </Text>
-      </Container>
+      <Mutation mutation={SUBSCRIBE}>
+        {(subscribe, { data }) => (
+          <Container>
+            {!data && <Form onSubmit={(variables) => subscribe({ variables })}>
+              <Container flex marginTop={2}>
+                <CandourConsumer>
+                  {theme =>
+                    <Fragment>
+                      <TextField field='name' placeholder='Full Name' style={{ ...theme.input.base, ...styles.input }} />
+                      <TextField field='email' placeholder='Email' style={{ ...theme.input.base, ...styles.input }} />
+                    </Fragment>
+                  }
+                </CandourConsumer>
+                <Button type='submit' disabled={data}>
+                  Join early adopters
+                </Button>
+              </Container>
+              <Container flexEnd>
+                <Text paddingTop>
+                  Pre-launch. Invites in a few weeks.
+                </Text>
+              </Container>
+            </Form>}
+            {data && <Text level={2} paddingTop>
+              <span role='img' aria-label=''>⚡️</span> Thank you {data.createSubscriber.name}! We will send you an invite in a few weeks.
+            </Text>}
+          </Container>
+        )}
+      </Mutation>
     </Container>
   </div>
 )
+
+const styles = {
+  input: {
+    marginRight: step(),
+  },
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { CandourProvider } from 'candour'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
 
 import registerServiceWorker from './registerServiceWorker'
 import App from './components/App'
@@ -8,9 +10,15 @@ import App from './components/App'
 import './theme/index.css'
 import candourTheme from './theme/candour'
 
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_GRAPHQL_API,
+})
+
 ReactDOM.render(
   <CandourProvider value={candourTheme}>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </CandourProvider>
 , document.getElementById('root'))
 registerServiceWorker()
