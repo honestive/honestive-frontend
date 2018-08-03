d084501195a80b8047329416134bde133f937a1a
Domas <domas.bitvinskas@me.com>
2018-08-03 18:41:05 +0300

    Use batch http link

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { CandourProvider } from 'candour'
-import ApolloClient from 'apollo-boost'
+import { ApolloClient } from 'apollo-client'
 import { ApolloProvider } from 'react-apollo'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { BatchHttpLink } from 'apollo-link-batch-http'
 
 import registerServiceWorker from './registerServiceWorker'
 import App from './components/App'
@@ -10,8 +12,13 @@ import App from './components/App'
 import './theme/index.css'
 import candourTheme from './theme/candour'
 
-const client = new ApolloClient({
+const link = new BatchHttpLink({
   uri: process.env.REACT_APP_GRAPHQL_API,
+});
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
 })
 
 ReactDOM.render(
